0c402422e4b8c9c9f30191cdf128dbfd68fbbb27
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-03 02:46:07 -0400

    Initial commit with the basic startup.

---
+++ Core/Entities/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
