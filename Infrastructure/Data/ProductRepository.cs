e77d7d455d2e9219f140773dadf2c821701b8a11
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-04 17:52:03 -0400

    Implemented the members, but not the logic.

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data
+{
+
+    public class ProductRepository : IProductRepository
+    {
+        public async Task<Product> GetProductByIdAsync(int id) { throw new System.NotImplementedException(); }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync() { throw new System.NotImplementedException(); }
+    }
+
+}
