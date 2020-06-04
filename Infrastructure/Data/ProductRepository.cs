6ec25f404f486b3eb3f8a8b71c109b809dc9d0ce
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-04 18:15:49 -0400

    Implemented the logic for the members of the interface.

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -2,15 +2,20 @@
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
 
     public class ProductRepository : IProductRepository
     {
-        public async Task<Product> GetProductByIdAsync(int id) { throw new System.NotImplementedException(); }
+        private readonly StoreContext _context;
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync() { throw new System.NotImplementedException(); }
+        public ProductRepository(StoreContext context) => _context = context;
+
+        public async Task<Product> GetProductByIdAsync(int id) => await  _context.Products.FindAsync(id);
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync() => await _context.Products.ToListAsync();
     }
 
 }
