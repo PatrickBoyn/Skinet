436ea5abf86abdded26455cac92973eb08b08397
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-05 15:50:42 -0400

    Inherited from BaseEntity.

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -2,9 +2,8 @@
 {
 
     // ReSharper disable once ClassNeverInstantiated.Global
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
     }
 
