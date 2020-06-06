875221cc1471b866d2fe104b05cff033b2cfabb2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-06 15:29:38 -0400

    Implemented the new methods in the repository.

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -16,6 +16,9 @@ namespace Infrastructure.Data
         public async Task<Product> GetProductByIdAsync(int id) => await  _context.Products.FindAsync(id);
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync() => await _context.Products.ToListAsync();
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync() => await _context.ProductBrands.ToListAsync();
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypeAsync() => await _context.ProductTypes.ToListAsync();
     }
 
 }
