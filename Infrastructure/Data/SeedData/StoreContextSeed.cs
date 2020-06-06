eec8c1ae8172758a1a4997a2ea9065f54762951b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-06 14:52:49 -0400

    Created a method for seeding data that is in the json files to the database.

--- Infrastructure/Data/SeedData/StoreContextSeed.cs
+++ Infrastructure/Data/SeedData/StoreContextSeed.cs
@@ -1,9 +1,77 @@
-﻿namespace Infrastructure.Data.SeedData
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data.SeedData
 {
 
     public class StoreContextSeed
     {
-        
+        public static async Task SeedAsync(StoreContext context, 
+                                           ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+  
+                    string brandsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/brands.json");
+
+                    List<ProductBrand> brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    foreach (ProductBrand brand in brands)
+                    {
+
+                        await context.ProductBrands.AddAsync(brand);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+                
+                if (!context.ProductTypes.Any())
+                {
+  
+                    string typesData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/types.json");
+
+                    List<ProductType> types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    foreach (ProductType type in types)
+                    {
+
+                        await context.ProductTypes.AddAsync(type);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+                
+                if (!context.Products.Any())
+                {
+  
+                    string productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+
+                    List<Product> products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                    foreach (Product product in products)
+                    {
+
+                        await context.Products.AddAsync(product);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                ILogger<StoreContextSeed> logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                
+                logger.LogError(e.Message);
+            }
+        }
     }
 
 }
