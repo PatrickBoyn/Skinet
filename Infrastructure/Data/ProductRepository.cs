75d528b09e587048c6e5e21c45e67f55c183ea84
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-06 15:43:46 -0400

    Added include statements so that the brands and types would show up.

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -13,9 +13,17 @@ namespace Infrastructure.Data
 
         public ProductRepository(StoreContext context) => _context = context;
 
-        public async Task<Product> GetProductByIdAsync(int id) => await  _context.Products.FindAsync(id);
+        public async Task<Product> GetProductByIdAsync(int id) => await  _context
+                                                                        .Products
+                                                                        .Include(p => p.ProductType)
+                                                                        .Include(p => p.ProductBrand)
+                                                                        .FirstOrDefaultAsync(p => p.Id == id);
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync() => await _context.Products.ToListAsync();
+        public async Task<IReadOnlyList<Product>> GetProductsAsync() => await _context.
+                                                                              Products
+                                                                             .Include(p => p.ProductType)
+                                                                             .Include(p => p.ProductBrand)
+                                                                             .ToListAsync();
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync() => await _context.ProductBrands.ToListAsync();
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypeAsync() => await _context.ProductTypes.ToListAsync();
