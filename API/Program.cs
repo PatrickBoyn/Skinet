a2d81e69c34e29bfe0803b4f32f00ac6a5b96765
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-06 14:27:35 -0400

    Automated running migrations whenever the program actually runs.

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,41 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace API
 {
 
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class Program
     {
-        public static void Main(string[] args) { CreateHostBuilder(args).Build().Run(); }
+        public static async Task Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+                ILoggerFactory loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    StoreContext context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                }
+                catch (Exception e)
+                {
+                    ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(e, "An error occured during migration.");
+                }
+            }
+            
+            await host.RunAsync();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
