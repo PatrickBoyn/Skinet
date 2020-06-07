a5144916dcf4b047c1a0c80a221a89fbf16d1b12
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-07 00:28:04 -0400

    Implemented the generic repo.

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,9 +1,21 @@
-﻿namespace Infrastructure.Data
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
 {
 
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
-        
+        private readonly StoreContext _context;
+
+        public GenericRepository(StoreContext context) => _context = context;
+
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+
+        public async Task<IReadOnlyList<T>> ListAllAsync() => await _context.Set<T>().ToListAsync();
     }
 
 }
