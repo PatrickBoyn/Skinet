4572cfc3a10a3f6c14442a7456e1852915c8cb65
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-08 14:59:23 -0400

    Added automapper as a service.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -19,6 +21,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(MappingProfiles));
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
              // You need both the interface and the concrete implementation.
             services.AddScoped<IProductRepository, ProductRepository>();
