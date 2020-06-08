ba6b91eea0b83b0656361219e699d52303043479
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-08 18:30:21 -0400

    This is just a test controller.

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,9 +1,50 @@
-﻿namespace API.Controllers
+﻿using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
 {
 
-    public class BuggyController
+    public class BuggyController : BaseApiController
     {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context) => _context = context;
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+           Product thing = _context.Products.Find(42);
+
+           if (thing == null)
+           {
+               return NotFound();
+           }
+            return Ok();
+        }
         
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            Product thing = _context.Products.Find(42);
+
+            string thingToReturn = thing.ToString();
+            
+            return Ok();
+        }
+        
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+        
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
     }
 
 }
