acaaf442a2add8d54afbb0271191e8404c2eff84
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-08 01:34:28 -0400

    Wrote a method to include the types and brands.

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,9 +1,15 @@
-﻿namespace Core.Specifications
+﻿using Core.Entities;
+
+namespace Core.Specifications
 {
 
-    public class ProductsWithTypesAndBrandsSpecification
+    public class ProductsWithTypesAndBrandsSpecification: BaseSpecification<Product>
     {
-        
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
     }
 
 }
