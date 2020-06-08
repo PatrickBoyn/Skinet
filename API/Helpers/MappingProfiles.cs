0cb607232bf3815f25e4728a3e71d463ea7cdcf8
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-08 14:55:36 -0400

    Created a map between the product and the dto.

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,9 +1,16 @@
-﻿namespace API.Helpers
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
 {
 
-    public class MappingProfiles
+    public class MappingProfiles : Profile
     {
-        
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>();
+        }
     }
 
 }
