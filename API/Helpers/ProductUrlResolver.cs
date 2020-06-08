41dd48a3996b11580e780aa026da146d42a78ea0
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-08 15:49:10 -0400

    Added the configuration that builds the url.

--- API/Helpers/ProductUrlResolver.cs
+++ API/Helpers/ProductUrlResolver.cs
@@ -1,9 +1,27 @@
-﻿namespace API.Helpers
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
 {
 
-    public class ProductUrlResolver
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
     {
-        
+        private readonly IConfiguration _configuration;
+
+        public ProductUrlResolver(IConfiguration configuration) => _configuration = configuration;
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember,
+                              ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _configuration["ApiUrl"] + source.PictureUrl;
+            }
+
+            return null;
+        }
     }
 
 }
