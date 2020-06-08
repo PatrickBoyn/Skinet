76352ab428378cfc620d206cbd59c3aaf6425b44
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-08 00:15:35 -0400

    Added the ability to add includes to the entities with the generic repository.

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -1,9 +1,27 @@
-﻿namespace Infrastructure.Data
+﻿using System.Linq;
+using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
 {
 
-    public class SpecificationEvaluator
+    public class SpecificationEvaluator<T> where T : BaseEntity
     {
-        
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, 
+                                             ISpecification<T> spec)
+        {
+            IQueryable<T> query = inputQuery;
+            
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
     }
 
 }
