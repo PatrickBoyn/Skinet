9b460868ce23173fdab8f04c2b793e23b441a8a1
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-09 18:07:58 -0400

    Added a property to the class to get an enumerable of errors.

--- API/Errors/ApiValidationErrorResponse.cs
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -1,9 +1,15 @@
-﻿namespace API.Errors
+﻿using System.Collections.Generic;
+
+namespace API.Errors
 {
 
-    public class ApiValidationErrorResponse
+    public class ApiValidationErrorResponse : ApiResponse
     {
-        
+        public ApiValidationErrorResponse() : base(400)
+        {
+        }
+
+        public IEnumerable<string> Errors { get; set; }
     }
 
 }
