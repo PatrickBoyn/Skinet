410d62cef5c91e760b9842e38a09a3a330c86d59
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-09 21:55:20 -0400

    Added the swagger stuff to the swagger extension class.

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -1,9 +1,37 @@
-﻿namespace API.Extensions
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
 {
 
-    public class SwaggerServiceExtensions
+    public static class SwaggerServiceExtensions
     {
-        
-    }
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+                                   {
+                                       c.SwaggerDoc("v1", new OpenApiInfo
+                                       {
+                                           Title = "Skinet Api",
+                                           Version = "v1"
+                                       });
+                                   });
 
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(x =>
+                             {
+                                 x.SwaggerEndpoint("/swagger/v1/swagger.json",
+                                                   "Skinet API v1");
+                             });
+
+            return app;
+        }
+    }
 }
