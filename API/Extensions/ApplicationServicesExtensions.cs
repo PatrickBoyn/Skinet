d5535de9138a2554ae1df7d42f4c8317465aba6f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-09 21:54:10 -0400

    Added some of the methods to the application services extension.

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -1,9 +1,55 @@
-﻿namespace API.Extensions
+﻿using System.Linq;
+using API.Errors;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
 {
 
-    public class ApplicationServicesExtensions
+    public static class ApplicationServicesExtensions
     {
-        
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            // You need both the interface and the concrete implementation.
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.Configure<ApiBehaviorOptions>(
+                                                   o =>
+                                                   {
+                                                       o.InvalidModelStateResponseFactory = context =>
+                                                                                            {
+                                                                                                string[] errors =
+                                                                                                    context
+                                                                                                       .ModelState
+                                                                                                       .Where(e =>
+                                                                                                                  e.Value
+                                                                                                                   .Errors
+                                                                                                                   .Count >
+                                                                                                                  0)
+                                                                                                       .SelectMany(x =>
+                                                                                                                       x.Value
+                                                                                                                        .Errors)
+                                                                                                       .Select(x =>
+                                                                                                                   x.ErrorMessage)
+                                                                                                       .ToArray();
+
+                                                                                                ApiValidationErrorResponse
+                                                                                                    errorResponse =
+                                                                                                        new
+                                                                                                            ApiValidationErrorResponse
+                                                                                                            {
+                                                                                                                Errors = errors
+                                                                                                            };
+
+                                                                                                return new
+                                                                                                    BadRequestObjectResult(errorResponse);
+                                                                                            };
+
+                                                   });
+            return services;
+        }
     }
 
 }
