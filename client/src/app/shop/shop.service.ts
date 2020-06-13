0275a74974785a56a77797260f07fe93a84fbd76
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-12 20:35:26 -0400

    Injected the HttpClient and used it to get products.

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {IPagination} from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getProducts = () => this.http.get<IPagination>(`${this.baseUrl}products?pageSize=50`);
 }
