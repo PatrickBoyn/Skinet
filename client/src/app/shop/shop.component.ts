1168a7dab9571609b5a8d35330ca7f367367b4c1
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-15 22:53:45 -0400

    Added an all option.

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -13,6 +13,9 @@ export class ShopComponent implements OnInit {
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
+  brandIdSelected: number;
+  typeIdSelected: number;
+
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
@@ -22,7 +25,7 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts = () => {
-    this.shopService.getProducts().subscribe(response => {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
       this.products = response.data;
     }, error => {
       console.log(error);
@@ -31,14 +34,23 @@ export class ShopComponent implements OnInit {
 
   getBrands = () => {
     this.shopService.getBrands().subscribe(response => {
-      this.brands = response;
+      this.brands = [{id: 0, name: 'All'}, ...response];
       }, error => console.log(error));
   }
 
   getTypes = () => {
     this.shopService.getTypes().subscribe(response => {
-      this.types = response;
+      this.types = [{id: 0, name: 'All'}, ...response];
     }, error => console.log(error));
   }
 
+  onBrandSelected = (brandId: number) => {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+}
+
+onTypeSelected = (typeId: number) => {
+    this.typeIdSelected = typeId;
+    this.getProducts();
+}
 }
