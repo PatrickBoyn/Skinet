8a37b58e9b3bfc375c65c59ee3b63743d8002a55
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-06-19 21:27:07 -0400

    Added the router module.

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,6 +4,7 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import {SharedModule} from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import {RouterModule} from '@angular/router';
 
 
 
@@ -12,9 +13,10 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     exports: [
         ShopComponent
     ],
-    imports: [
-        CommonModule,
-        SharedModule
-    ]
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
 })
 export class ShopModule { }
